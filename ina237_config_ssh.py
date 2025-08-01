ab3b7b049356d61522ea0584874bb4c2af23e06c
awolfman <35307733+awolfman@users.noreply.github.com>
2025-08-01 10:11:39 +0300

    Update ina237_config_ssh.py
    Добавлена обработка ошибок

--- ina237_config_ssh.py
+++ ina237_config_ssh.py
@@ -2,18 +2,20 @@
 
 import paramiko
 import sys
+import socket
 import time
 import re
-#
+
 host = sys.argv[1]
 bus = sys.argv[2]
 user = sys.argv[3]
 password=sys.argv[4]
-#
+
 # Настройки для INA237 
 i2caddr = [0x4a, 0x4b, 0x4e, 0x4f]
 _get = i2cget
 _set = i2cset
+
 # Регистры
 REG_CONFIG = 0x00
 REG_ADC_CONFIG = 0x01
@@ -32,6 +34,7 @@ REG_TEMP_LIMIT = 0x10
 REG_PWR_LIMIT = 0x11
 REG_MANUFACTURER_ID = 0x3E
 REG_DEVICE_ID = 0x3F
+
 # Значения для калибровки и вычислений
 max_voltage = 75
 shunt_resistance = 0.005
@@ -40,44 +43,72 @@ current_lsb = max_current/32768
 power_lsb = 50 * current_lsb # 50 times current LSB
 calibration_value = int((819.2 * 10**6) * (current_lsb * shunt_resistance))
 calibration_value &= 0x7FFF # Ensure bit 15 is reserved (set to 0)
-#
+
 print ("Калибровочное значение:", hex(calibration_value))
-#
+
 with paramiko.SSHClient() as client:
-#
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host, username=user, password=password)
+
+    try:
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(host, username=user, password=password)
+    except (socket.gaierror, socket.error, socket.timeout, TimeoutError, IOError, paramiko.ssh_exception.NoValidConnectionsError):
+        print("Could not connect to %s \n" % host)
+        sys.exit(1) 
+
     i = 0
     while i < 4:
-        print (f"Фидер {i+1}:" )
-# Записываем калибровочное значение
-        (stdin, stdout, stderr) = client.exec_command(f'{_set} -y {bus} {i2caddr[i]} {REG_SHUNT_CAL} {calibration_value} w')
-        time.sleep(0.1)
-# Проверяем, что всё записалось правильно
-        (stdin, stdout, stderr) = client.exec_command(f'{_get} -y {bus} {i2caddr[i]} {REG_SHUNT_CAL} w')
-        print("Проверяем паравильность записи калибровки:", stdout.read().decode() )
-#
-        (stdin, stdout, stderr) = client.exec_command(f'{_get} -y {bus} {i2caddr[i]} {REG_VBUS} w')
-        raw_vbus = stdout.read().decode()
-        vbus = int( ''.join(char for char in raw_vbus if char.isalnum()) , 16 )
-        print ("Vin = {:.2f} V".format(vbus * 0.003125 ) )
-#
-        (stdin, stdout, stderr) = client.exec_command(f'{_get} -y {bus} {i2caddr[i]} {REG_CURRENT} w')
-        raw_current = stdout.read().decode()
-        current = int( ''.join(char for char in raw_current if char.isalnum()) , 16 )
-        print ("Iin = {:.2f} A".format(current * current_lsb ) )
-#
-        (stdin, stdout, stderr) = client.exec_command(f'{_get} -y {bus} {i2caddr[i]} {REG_POWER} w')
-        raw_power = stdout.read().decode()
-        power = int( ''.join(char for char in raw_power if char.isalnum()) , 16 )
-        power &= 0x00FFFFFF # Ensure bit 31-24 is reserved
-        print ("Pin = {:.2f} W".format(power_lsb * power ) )
-#
-        (stdin, stdout, stderr) = client.exec_command(f'{_get} -y {bus} {i2caddr[i]} {REG_DIETEMP} w')
-        raw_temperature = stdout.read().decode()
-        temperature = int( ''.join(char for char in raw_temperature if char.isalnum()) , 16 )
-        temperature &= 0xFFF0  # Ensure bits 0-3 are reserved (set to 0)
-        print ("Temperature = {:.2f} °C".format((temperature >> 4) * 0.125 ) )
-        print ('*****************')
-        i = i + 1
+        try:
+            print (f"Фидер {i+1}:" )
+            # Записываем калибровочное значение
+            (stdin, stdout, stderr) = client.exec_command(f'i2cset -y 17 {i2caddr[i]} {REG_SHUNT_CAL} {calibration_value} w')
+            time.sleep(0.1)
+            (stdin, stdout, stderr) = client.exec_command(f'i2cget -y 17 {i2caddr[i]} {REG_SHUNT_CAL} w')
+            try:
+                for line in stdout:
+                    print("Проверяем паравильность записи калибровки:", stdout.read().decode() )
+            except line in stderr:
+                print (line)
+                sys.exit(1)
+
+            (stdin, stdout, stderr) = client.exec_command(f'i2cget -y 17 {i2caddr[i]} {REG_VBUS} w')
+            for line in stderr:
+                print (line)
+                sys.exit(1)
+            raw_vbus = stdout.read().decode()
+            vbus = int( ''.join(char for char in raw_vbus if char.isalnum()) , 16 )
+            print ("Vin = {:.2f} V".format(vbus * 0.003125 ) )
+
+            (stdin, stdout, stderr) = client.exec_command(f'i2cget -y 17 {i2caddr[i]} {REG_CURRENT} w')
+            for line in stderr:
+                print (line)
+                sys.exit(1)
+            raw_current = stdout.read().decode()
+            current = int( ''.join(char for char in raw_current if char.isalnum()) , 16 )
+            print ("Iin = {:.2f} A".format(current * current_lsb ) )
+
+            (stdin, stdout, stderr) = client.exec_command(f'i2cget -y 17 {i2caddr[i]} {REG_POWER} w')
+            for line in stderr:
+                print (line)
+                sys.exit(1)
+            raw_power = stdout.read().decode()
+            power = int( ''.join(char for char in raw_power if char.isalnum()) , 16 )
+            power &= 0x00FFFFFF # Ensure bit 31-24 is reserved
+            print ("Pin = {:.2f} W".format(power_lsb * power ) )
+
+            (stdin, stdout, stderr) = client.exec_command(f'i2cget -y 17 {i2caddr[i]} {REG_DIETEMP} w')
+            for line in stderr:
+                print (line)
+                sys.exit(1)
+            raw_temperature = stdout.read().decode()
+            temperature = int( ''.join(char for char in raw_temperature if char.isalnum()) , 16 )
+            temperature &= 0xFFF0  # Ensure bits 0-3 are reserved (set to 0)
+            print ("Temperature = {:.2f} °C".format((temperature >> 4) * 0.125 ) )
+
+            print ('*****************')
+
+            i = i + 1
+        except (socket.gaierror, socket.error, socket.timeout, TimeoutError, IOError, paramiko.ssh_exception.NoValidConnectionsError) as error:
+            print(error)
+            sys.exit(1)
+
     client.close()
